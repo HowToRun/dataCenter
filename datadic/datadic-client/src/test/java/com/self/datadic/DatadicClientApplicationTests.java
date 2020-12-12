7437723b8ded337401717a39f0e2d1c35b240b9f
yihan.hu <yihan.hu@percent.cn>
2020-12-12 17:56:49 +0800

    delete IP

---
+++ datadic/datadic-client/src/test/java/com/self/datadic/DatadicClientApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.self.datadic;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DatadicClientApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
